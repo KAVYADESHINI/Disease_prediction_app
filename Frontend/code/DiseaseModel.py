b72cc1645bd0fca1dfc91438b8b1c156b973b8b4
Kavya Deshini <kavyadeshini1224@gmail.com>
2026-05-01 11:13:06 +0530

    Update DiseaseModel.py

--- Frontend/code/DiseaseModel.py
+++ Frontend/code/DiseaseModel.py
@@ -1,70 +1,22 @@
-import xgboost as xgb
-import pandas as pd
 import os
+import pandas as pd
+import xgboost as xgb
+
+# Add these two lines at the top (outside the class)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, '..', 'data')
-class DiseaseModel:
 
+class DiseaseModel:
     def __init__(self):
-        self.all_symptoms = None
         self.symptoms = None
         self.pred_disease = None
         self.model = xgb.XGBClassifier()
-        self.diseases = self.disease_list('os.path.join(DATA_DIR, 'dataset.csv'))
+        self.diseases = self.disease_list(os.path.join(DATA_DIR, 'dataset.csv'))
 
     def load_xgboost(self, model_path):
         self.model.load_model(model_path)
 
-    def save_xgboost(self, model_path):
-        self.model.save_model(model_path)
-
-    def predict(self, X):
-        self.symptoms = X
-        disease_pred_idx = self.model.predict(self.symptoms)
-        self.pred_disease = self.diseases[disease_pred_idx].values[0]
-        disease_probability_array = self.model.predict_proba(self.symptoms)
-        disease_probability = disease_probability_array[0, disease_pred_idx[0]]
-        return self.pred_disease, disease_probability
-
-    
-    def describe_disease(self, disease_name):
-
-        if disease_name not in self.diseases:
-            return "That disease is not contemplated in this model"
-        
-        # Read disease dataframe
-        desc_df = pd.read_csv('data/symptom_Description.csv')
-        desc_df = desc_df.apply(lambda col: col.str.strip())
-
-        return desc_df[desc_df['Disease'] == disease_name]['Description'].values[0]
-
-    def describe_predicted_disease(self):
-
-        if self.pred_disease is None:
-            return "No predicted disease yet"
-
-        return self.describe_disease(self.pred_disease)
-    
-    def disease_precautions(self, disease_name):
-
-        if disease_name not in self.diseases:
-            return "That disease is not contemplated in this model"
-
-        # Read precautions dataframe
-        prec_df = pd.read_csv('data/symptom_precaution.csv')
-        prec_df = prec_df.apply(lambda col: col.str.strip())
-
-        return prec_df[prec_df['Disease'] == disease_name].filter(regex='Precaution').values.tolist()[0]
-
-    def predicted_disease_precautions(self):
-
-        if self.pred_disease is None:
-            return "No predicted disease yet"
-
-        return self.disease_precautions(self.pred_disease)
-
     def disease_list(self, kaggle_dataset):
-
         df = pd.read_csv(os.path.join(DATA_DIR, 'clean_dataset.tsv'), sep='\t')
         # Preprocessing
         y_data = df.iloc[:,-1]
