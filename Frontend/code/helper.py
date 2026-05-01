629e10a3ea113cc51043723bf552b15e2e9ac923
Kavya Deshini <kavyadeshini1224@gmail.com>
2026-05-01 11:23:34 +0530

    Update helper.py

--- Frontend/code/helper.py
+++ Frontend/code/helper.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+import os
+import pandas as pd
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, '..', 'data')
 
 def prepare_symptoms_array(symptoms):
+    df = pd.read_csv(os.path.join(DATA_DIR, 'clean_dataset.tsv'), sep='\t')
     '''
     Convert a list of symptoms to a ndim(X) (in this case 131) that matches the
     dataframe used to train the machine learning model
@@ -11,7 +16,6 @@ def prepare_symptoms_array(symptoms):
     - X (np.array) = X values ready as input to ML model to get prediction
     '''
     symptoms_array = np.zeros((1,133))
-    df = pd.read_csv('data/clean_dataset.tsv', sep='\t')
     
     for symptom in symptoms:
         symptom_idx = df.columns.get_loc(symptom)
